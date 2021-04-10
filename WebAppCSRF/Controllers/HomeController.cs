3086cf1067eb1acbbba534b63dbbd9f3f17d8687
baranau_an <andrei_baranau1@epam.com>
2021-04-10 22:58:41 +0300

    Added project to repo.

---
+++ WebAppCSRF/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppCSRF.Models;
+
+namespace WebAppCSRF.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            return Ok($"{nameof(Employee)} created!");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebAppCSRF/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAppCSRF.Models
+{
+    public class Employee
+    {
+        public string EmployeeName { get; set; }
+        public uint EmployeeAge { get; set; }
+        public uint DepartmentId { get; set; }
+    }
+}
